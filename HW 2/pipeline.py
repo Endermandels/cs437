5d67487cea63794c8e07db023ce71b6dcc29f0ab
Endermandels <elijahdelavar@gmail.com>
2024-09-24 11:00:41 -0700

    Plot numeric data, Achieve 82% r2

--- HW 2/pipeline.py
+++ HW 2/pipeline.py
@@ -4,13 +4,16 @@ CS 437
 Elijah Delavar
 """
 
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+TARGET = 'SalePrice'
 
 class SelectColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -26,11 +29,14 @@ class SelectColumns(BaseEstimator, TransformerMixin):
 def main():
     data = pd.read_csv('AmesHousing.csv')
 
+    data = data.fillna(0)
+
+    numeric_columns = data.select_dtypes(include=['float64', 'int64']).columns
+    numeric_columns = numeric_columns.drop([TARGET, 'Order', 'PID', 'BsmtFin SF 2', 'Low Qual Fin SF', '3Ssn Porch', 'Pool Area', 'Misc Val', 'Mo Sold', 'Yr Sold'])
+
     grid = {
         'column_select__columns': [
-            ['Gr Liv Area']
-            , ['Overall Qual']
-            , ['Gr Liv Area', 'Overall Qual']
+            numeric_columns
         ],
         'linear_regression': [
             TransformedTargetRegressor(
@@ -63,13 +69,29 @@ def main():
     pipe = Pipeline(steps)
     search = GridSearchCV(pipe, grid, scoring='r2', n_jobs=-1)
 
-    xs = data.drop(columns=['SalePrice'])
-    ys = data['SalePrice']
+    xs = data.drop(columns=[TARGET])
+    ys = data[TARGET]
 
     search.fit(xs, ys)
-    r_squared = search.best_score_
 
-    print(f'R-Squared: {r_squared}')
+    print(search.best_score_)
+    print(search.best_params_)
+
+def data_corr():
+    data = pd.read_csv('AmesHousing.csv')
+    
+    numeric_columns = data.select_dtypes(include=['float64', 'int64']).columns
+    numeric_columns = numeric_columns.drop(TARGET)
+
+    for col in numeric_columns:
+        plt.figure(figsize=(6,4))
+        plt.scatter(data[col], data[TARGET], alpha=0.5)
+        plt.title(f'{col} vs {TARGET}')
+        plt.xlabel(col)
+        plt.ylabel(TARGET)
+        plt.grid(True)
+        plt.show()
 
 if __name__ == '__main__':
     main()
+    # data_corr()
