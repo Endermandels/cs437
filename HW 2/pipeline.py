90973b3bedc55ed38a058e7e1e017fb97125bb35
Endermandels <elijahdelavar@gmail.com>
2024-09-26 11:47:28 -0700

    Clean up code, add comments

--- HW 2/pipeline.py
+++ HW 2/pipeline.py
@@ -10,11 +10,8 @@ from sklearn.compose import TransformedTargetRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 import numpy as np
-import sys
 
 TARGET = 'SalePrice'
 
@@ -53,13 +50,16 @@ def main():
     data[categorical_columns] = data[categorical_columns].fillna('Missing')
     data[numeric_columns] = data[numeric_columns].fillna(0)
     
+    # Encode categorical columns
     encoder = OneHotEncoder(sparse_output=False, drop='first')
     encoded_columns = encoder.fit_transform(data[categorical_columns])
     encoded_df = pd.DataFrame(encoded_columns, columns=encoder.get_feature_names_out(categorical_columns))
 
+    # Drop the original categorical data and add the encoded data
     data = data.drop(categorical_columns, axis=1)
     data = pd.concat([data, encoded_df], axis=1)
 
+    # Get the correct feature names from the encoder
     categorical_columns = encoder.get_feature_names_out(categorical_columns)
 
     grid = {
@@ -68,23 +68,27 @@ def main():
         ],
         'transform_data__func': [
             lambda x: np.where(x > 0, np.sqrt(x), 0)
+            # , lambda x: x
+            # , lambda x: np.where(x > 1, np.log(x), 0)
+            # , np.square
+            # , np.exp
         ],
         'linear_regression': [
             TransformedTargetRegressor(
                 LinearRegression(n_jobs=-1),
-                func = lambda x: x,
-                inverse_func = lambda x: x
-            ),
-            TransformedTargetRegressor(
-                LinearRegression(n_jobs=-1),
                 func = np.sqrt,
                 inverse_func = np.square
-            ),
-            TransformedTargetRegressor(
-                LinearRegression(n_jobs=-1),
-                func = np.log,
-                inverse_func = np.exp
             )
+            # , TransformedTargetRegressor(
+            #     LinearRegression(n_jobs=-1),
+            #     func = lambda x: x,
+            #     inverse_func = lambda x: x
+            # )
+            # , TransformedTargetRegressor(
+            #     LinearRegression(n_jobs=-1),
+            #     func = np.log,
+            #     inverse_func = np.exp
+            # )
         ]
     }
     
@@ -105,48 +109,8 @@ def main():
 
     search.fit(xs, ys)
 
-    print(search.best_score_)
+    print(search.best_score_) # Consistently gets 0.86423 r2
     print(search.best_params_)
-
-def data_corr():
-    data = pd.read_csv('AmesHousing.csv')
-    
-    categorical_columns = data.select_dtypes(include=['object', 'category']).columns
-
-    numeric_columns = data.select_dtypes(include=['float64', 'int64']).columns
-    numeric_columns = numeric_columns.drop([TARGET, 'Order', 'PID', 'BsmtFin SF 2',
-                                            'Low Qual Fin SF', '3Ssn Porch', 'Pool Area',
-                                            'Misc Val', 'Mo Sold', 'Yr Sold'])
-
-    data[categorical_columns] = data[categorical_columns].fillna('Missing')
-    data[numeric_columns] = data[numeric_columns].fillna(0)
-    
-    encoder = OneHotEncoder(sparse_output=False, drop='first')
-    encoded_columns = encoder.fit_transform(data[categorical_columns])
-    encoded_df = pd.DataFrame(encoded_columns, columns=encoder.get_feature_names_out(categorical_columns))
-
-    data = data.drop(categorical_columns, axis=1)
-    data = pd.concat([data, encoded_df], axis=1)
-    
-    categorical_columns = encoder.get_feature_names_out(categorical_columns)
-    
-    assert data.isna().sum().sum() == 0
-    
-    for col in categorical_columns:
-        plt.figure(figsize=(6,4))
-        plt.scatter(data[col], data[TARGET], alpha=0.5)
-        plt.title(f'{col} vs {TARGET}')
-        plt.xlabel(col)
-        plt.ylabel(TARGET)
-        plt.grid(True)
-        plt.show()
-    
-    # correlation_matrix = data[numeric_columns].corr()
-
-    # plt.figure(figsize=(12,8))
-    # sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
-    # plt.show()
 
 if __name__ == '__main__':
     main()
-    # data_corr()
