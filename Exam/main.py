e145d5d1eec2f50717addcbd72ec164426215b46
Endermandels <elijahdelavar@gmail.com>
2024-10-02 13:29:59 -0700

    Split data into train test data frames

--- Exam/main.py
+++ Exam/main.py
@@ -1,5 +1,6 @@
 # CC 1 (Code Cel 1)
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 import numpy as np
@@ -12,14 +13,20 @@ data = data.drop(columns=['index', 'Rank', 'Game Title', 'North America', 'Europ
 
 categorical_columns = data.select_dtypes(include=['object', 'category']).columns
 numeric_columns = data.select_dtypes(include=['float64', 'int64']).columns
-# data[categorical_columns] = data[categorical_columns].fillna('Missing')
-# data[numeric_columns] = data[numeric_columns].fillna(0)
-
 categorical_data = pd.get_dummies(data[categorical_columns], dtype=np.int64)
 
 data = data.drop(categorical_columns, axis=1)
 data = pd.concat([data, categorical_data], axis=1)
-
 data.fillna(value=0, inplace=True)
 
-print (data)
+xs = data.drop(columns=['Global'])
+ys = data['Global']
+
+x_train, x_test, y_train, y_test = train_test_split(xs, ys, train_size=0.7)
+
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
+
+
