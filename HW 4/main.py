c27d6756929eeef8df2a074d8b473ec8bf21eb10
Endermandels <elijahdelavar@gmail.com>
2024-11-04 16:34:43 -0800

    Print each cluster's results

--- HW 4/main.py
+++ HW 4/main.py
@@ -11,29 +11,66 @@ from sklearn.cluster import KMeans
 import pandas as pd
 
 def printResults(df: pd.DataFrame, pipe: Pipeline):
-    for type in set(df['Type 1']):
+    types = set(df['Type 1'])
+    clusters = {}
+    
+    # Show cluster scores
+    for type in types:
         print(type)
         print('-----------')
-        data = df[df['Type 1'] == type].drop(columns=['Type 1'])
-        best_choice, best_score = optimize_n_clusters(range(2,min(data.shape[0], 15)), data, pipe)
+        
+        data = df[df['Type 1'] == type].drop(columns=['Type 1', 'Name'])
+        
+        best_choice, best_score, best_prediction = \
+            optimize_n_clusters(range(2,min(data.shape[0], 15)), data, pipe)
+            
+        clusters[type] = best_prediction
+        
         print('Best number of clusters:', best_choice)
         print('Best score:', best_score)
+        print()
+
+    # Show cluster elements and means
+    for type in types:
+        print(type)
+        print('-----------')
+        
+        data = df[df['Type 1'] == type].drop(columns=['Type 1'])
+        
+        # Put cluster into data
+        data['Cluster'] = clusters[type]
+        
+        # Print data grouped by cluster
+        groups = data.groupby('Cluster')
+        for cluster_num, cluster_data in groups:
+            print(f"Cluster {cluster_num}")
+            print(cluster_data.drop(columns=['Cluster']).to_string())
+            print('Mean HP:', cluster_data['HP'].mean())
+            print('Mean Attack:', cluster_data['Attack'].mean())
+            print('Mean Defend:', cluster_data['Defense'].mean())
+            print('Mean Sp. Attack:', cluster_data['Sp. Atk'].mean())
+            print('Mean Sp. Defense:', cluster_data['Sp. Def'].mean())
+            print('Mean Speed:', cluster_data['Speed'].mean())
+            print()
         print()
 
 def optimize_n_clusters(r: range, data: pd.DataFrame, pipe: Pipeline):
     best_choice = r.start
     best_silhouette = float('-inf')
+    best_prediction = None
     
     for n_clusters in r:
         pipe.set_params(cluster__n_clusters=n_clusters)
-        score = silhouette_score(data, pipe.fit_predict(data))
+        prediction = pipe.fit_predict(data)
+        score = silhouette_score(data, prediction)
         
         print(f'{n_clusters} clusters: {score}')
         
         if score > best_silhouette:
             best_silhouette = score
             best_choice = n_clusters
-    return best_choice, best_silhouette # we didn't have to return the score
+            best_prediction = prediction
+    return best_choice, best_silhouette, best_prediction
 
 def createPipeline(df: pd.DataFrame):
     steps = [
@@ -45,7 +82,7 @@ def createPipeline(df: pd.DataFrame):
 
 def getData(fn: str):
     df = pd.read_csv(fn)
-    df = df.drop(columns=['#', 'Name', 'Total', 'Generation', 'Legendary', 'Type 2'])
+    df = df.drop(columns=['#', 'Total', 'Generation', 'Legendary', 'Type 2'])
     return df
 
 def main():
